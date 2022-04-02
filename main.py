bd01e973074c0f496cedc62b6f84ade87525f155
avoss19 <voss1167@gmail.com>
2022-04-01 19:37:40 -0500

    fin

--- main.py
+++ main.py
@@ -61,6 +61,10 @@ class Instr():
 		self.cycles = 0 # cycles in current state
 
 		self.result = None
+		
+		self.issued = False
+		self.dispatched = False
+		self.broadcasted = False
 
 	def getCycles(self):
 		# Return cycles for OP codes
@@ -77,50 +81,83 @@ class Instr():
 			case 3:
 				return 40
 
-	def issue(self, depWait = False):
+	def issue(self, addRes, multRes, rf, rat, cycle, depWait = False):
+		# Rewrite
+		if(not self.issued):
+			if(rat.getReg(self.src1) == None):
+				t1 = None
+				v1 = rf.getReg(self.src1)
+			else:
+				t1 = rat.getReg(self.src1)
+				v1 = None
+
+			if(rat.getReg(self.src2) == None):
+				t2 = None
+				v2 = rf.getReg(self.src2)
+			else:
+				t2 = rat.getReg(self.src2)
+				v2 = None
+
+			if(self.op < 2):
+				ratRS = addRes.addItem(self.op, t1, t2, v1, v2, cycle)
+			else:
+				ratRS = multRes.addItem(self.op, t1, t2, v1, v2, cycle)
+
+			rat.setReg(self.rst, ratRS)
+			
+			self.issued = True
+		
 		if(not depWait):
 			self.state = self.state + 1
-
-	def dispatch(self):
+			
+	def dispatch(self, rf, rat, broadcastWait = False):
 		self.cycles = self.cycles + 1
-		if(self.getCycles() == self.cycles):
+				
+		if(not self.dispatched):
+			match self.op:
+				case 0:
+					self.result = rf.getReg(self.src1) + rf.getReg(self.src2)
+				case 1:
+					self.result = rf.getReg(self.src1) - rf.getReg(self.src2)
+				case 2:
+					self.result = rf.getReg(self.src1) * rf.getReg(self.src2)
+				case 3:
+					self.result = rf.getReg(self.src1) / rf.getReg(self.src2)
+		
+		if(self.getCycles() <= self.cycles and not broadcastWait):
 			self.state = self.state + 1
 
 	def broadcast(self, rf):
-		pass
-		#rf.setReg(self.rst, self.result)
+		if(not self.broadcasted):
+			rf.setReg(self.rst, self.result)
+		self.broadcasted = True
 
 	def run(self, rf, rat, addReserve, multReserve, cycle, depWait = False, broadcastWait = False):
 		match self.state:
 			case 0:
-				self.issue(depWait)
+				self.issue(addReserve, multReserve, rf, rat, cycle, depWait)
 			case 1:
-				self.dispatch(broadcastWait)
+				self.dispatch(rf, rat, broadcastWait)
 			case 2:
 				self.broadcast(rf)
 
-		# Check for raw dep
-
-		# Execute instr
-		match self.op:
-			case 0:
-				""
-				# ratRS = addReserve.addItem(0, None, None, None, None, cycle)
-				# rat.setReg(self.rst, ratRS) # update RAT
-				#rf.setReg(self.rst, getReg(self.src1) + getReg(self.src2))
-			case 1:
-				""
-				# rf.setReg(self.rst, getReg(self.src1) - getReg(self.src2))
-			case 2:
-				""
-				# rf.setReg(self.rst, getReg(self.src1) * getReg(self.src2))
-			case 3:
-				""
-				# rf.setReg(self.rst, getReg(self.src1) / getReg(self.src2))
-
-
 class InstrQueue():
 	# IQ
+	def __str__(self):
+		instrStr = "Instruction Queue:\n"
+		for i in self.instrs:
+			match i.op:
+				case 0:
+					op = "Add"
+				case 1:
+					op = "Sub"
+				case 2:
+					op = "Mul"
+				case 3:
+					op = "Div"
+			instrStr = instrStr + "%s, R%d, R%d, R%d\n" % (op, i.rst, i.src1, i.src2)
+		return str(instrStr)
+	
 	def __init__(self, instrs):
 		self.pc = 0
 		self.instrs = instrs
@@ -136,7 +173,6 @@ class InstrQueue():
 		if(self.pc == len(self.instrs)):
 			self.pc = 0
 		instr = self.instrs[self.pc].copy()
-		instr.run(rf, rat, addReserve, multReserve, cycle, depWait)
 		self.pc = self.pc + 1
 
 		return instr
@@ -146,6 +182,7 @@ class RF():
 		return str(self.rf)
 
 	def __init__(self, rf = None, size = 8):
+		self.size = size
 		if rf != None:
 			self.rf = rf
 		else:
@@ -164,7 +201,8 @@ class RAT(RF):
 
 	def __init__(self, size = 8):
 		# RAT initilizes where R0 = RS0, R1 = RS1, ..., RN = RSN
-		super().__init__(range(0, size))
+		#super().__init__([i for i in range(0, size)])
+		super().__init__([None] * size)
 
 class Reserve():
 	def __init__(self, size, rsIndex = 0):
@@ -172,12 +210,12 @@ class Reserve():
 		self.num = 0 # number of items
 		self.rsIndex = rsIndex
 
-		self.op = [None]
-		self.t1 = [None]
-		self.t2 = [None]
-		self.v1 = [None]
-		self.v2 = [None]
-		self.busy = [None]
+		self.op = [None] * size
+		self.t1 = [None] * size
+		self.t2 = [None] * size
+		self.v1 = [None] * size
+		self.v2 = [None] * size
+		self.busy = [None] * size
 
 	def set(self, op, t1, t2, v1, v2, busy, index):
 		self.op[index] = op
@@ -188,24 +226,26 @@ class Reserve():
 		self.busy[index] = busy
 
 	def addItem(self, op, t1, t2, v1, v2, busy):
-		if(num == 0):
-			self.op[index] = [op]
-			self.t1[index] = [t1]
-			self.t2[index] = [t2]
-			self.v1[index] = [v1]
-			self.v2[index] = [v2]
-			self.busy[index] = [busy]
+		index = self.num % self.size
 
-		index = num % size
-		self.op[index].append(op)
-		self.t1[index].append(t1)
-		self.t2[index].append(t2)
-		self.v1[index].append(v1)
-		self.v2[index].append(v2)
-		self.busy[index].append(busy)
-		self.num = num + 1
+		if(self.num < self.size):
+			self.op[self.num] = [op]
+			self.t1[self.num] = [t1]
+			self.t2[self.num] = [t2]
+			self.v1[self.num] = [v1]
+			self.v2[self.num] = [v2]
+			self.busy[self.num] = [busy]
+		else:
+			self.op[index].append(op)
+			self.t1[index].append(t1)
+			self.t2[index].append(t2)
+			self.v1[index].append(v1)
+			self.v2[index].append(v2)
+			self.busy[index].append(busy)
+			
+		self.num = self.num + 1
 
-		return self.rsIndex + self.index
+		return self.rsIndex + index
 
 
 	def getSize():
@@ -217,17 +257,36 @@ class Reserve():
 def printRFRAT(rf, rat):
 	print("\tRF\t\tRAT")
 	for i in range(0, len(rf.rf)):
-		print("%d:\t%d\t\tRS%d" % (i, rf.rf[i], rat.rf[i]))
+		ratStr = ""
+		if(rat.rf[i] != None):
+			ratStr = "RS" + str(rat.rf[i])
+		print("%d:\t%d\t\t%s" % (i, rf.rf[i], ratStr))
 
+def cleanArrStr(arrStr):
+	arrStr = arrStr.replace("None", "N")
+	arrStr = arrStr.replace("[", "")
+	arrStr = arrStr.replace("]", "")
+	return arrStr
+		
 def printReserves(addRes, multRes):
-	print("\t\t\tBusy\t\t\tOP\t\t\tVj\t\t\tVk\t\t\tQj\t\t\tQk\t\t\tDisp")
+	print("{:<14} {:<14} {:<14} {:<14} {:<14} {:<14} {:<14} {:<14}".format("", "Busy", "OP", "Vj", "Vk", "Qj", "Qk", "Disp/Cycle"))
+	
+	# Print addRes
 	for i in range(0, addRes.size):
 		if(addRes.op[0] != None):
-			busy = [1]*len(addRes.busy)
+			busy = [1]*len(addRes.busy[i])
 		else:
 			busy = [0]
-		print("RS%d\t\t\t%s\t\t\t%s\t\t\t%s\t\t\t%s\t\t\t%s\t\t\t%s\t\t\t%s" % (addRes.rsIndex + i, busy, addRes.op, addRes.v1, addRes.v2, addRes.t1, addRes.t2, addRes.busy))
+		print("{:<14} {:<14} {:<14} {:<14} {:<14} {:<14} {:<14} {:<14}".format("RS" + str(addRes.rsIndex + i), cleanArrStr(str(busy)),  cleanArrStr(str(addRes.op[i])),  cleanArrStr(str(addRes.v1[i])),  cleanArrStr(str(addRes.v2[i])),  cleanArrStr(str(addRes.t1[i])),  cleanArrStr(str(addRes.t2[i])),  cleanArrStr(str(addRes.busy[i]))))
 
+	# Print multRes
+	for i in range(0, multRes.size):
+		if(multRes.op[0] != None):
+			busy = [1]*len(multRes.busy[i])
+		else:
+			busy = [0]
+		print("{:<14} {:<14} {:<14} {:<14} {:<14} {:<14} {:<14} {:<14}".format("RS" + str(multRes.rsIndex + i), cleanArrStr(str(busy)),  cleanArrStr(str(multRes.op[i])),  cleanArrStr(str(multRes.v1[i])),  cleanArrStr(str(multRes.v2[i])),  cleanArrStr(str(multRes.t1[i])),  cleanArrStr(str(multRes.t2[i])),  cleanArrStr(str(multRes.busy[i]))))	
+	
 def instrParse(instr):
 	return Instr(instr[0], instr[1], instr[2], instr[3])
 
@@ -242,11 +301,11 @@ def sim(file):
 	numCycles = int(data[1])
 
 	# Read instr
-	instr = data[2 : int(numInstr) + 2]
-	instr = [instrParse(x[:-1].split()) for x in instr] # clean data (remove \n)
+	instrs = data[2 : int(numInstr) + 2]
+	instrs = [instrParse(x[:-1].split()) for x in instrs] # clean data (remove \n)
 
 	# Create Instruction Queue
-	iq = InstrQueue(instr)
+	iq = InstrQueue(instrs)
 
 	# Read init RF values
 	rf = data[int(numInstr) + 2 : len(data)]
@@ -261,32 +320,35 @@ def sim(file):
 
 	# Init vars
 	cycle = 0 # cpu cycle
-	#rawDep = []
 
 	# Sim loop
-	instr = []
+	instrs = []
 	for i in range(0, numCycles):
-		instr.append(iq.execNextInstr(rf, rat, addRes, multRes, cycle))
-		rawDep = []
-		for j in instr:
-			if(j.state != 2):
-				rawDep.append(j.rst)
-			print(j.rst)
-			print(rawDep)
-			depWait1 = rawDep.count(j.src1) - 1 > 0
-			depWait2 = rawDep.count(j.src2) - 1 > 0
-			print("depWait:" + str(bool(depWait1)) + str(bool(depWait1)))
-			j.run(rf, rat, addRes, multRes, cycle, depWait = (depWait1 or depWait2))
-		#iq.issueNextInstr
+		instrs.append(iq.execNextInstr(rf, rat, addRes, multRes, cycle))
+		rsWaitBroad = []
+		firstElement = True
+		for instr in instrs:
+			depWait1 = rsWaitBroad.count(instr.src1) > 0
+			depWait2 = rsWaitBroad.count(instr.src2) > 0
+			
+			rsWaitBroad.append(instr.rst)
+						
+			instr.run(rf, rat, addRes, multRes, cycle, (depWait1 or depWait2), broadcastWait = not firstElement)
+			if(instr.broadcasted == True and firstElement):
+				instrs.pop(0)
+			
+			firstElement = False
 		cycle = cycle + 1
 
 	printReserves(addRes,multRes)
+	print()
 	printRFRAT(rf, rat)
-
+	print()
+	print(iq)
 
 if __name__ == "__main__":
 	if(len(sys.argv) > 1):
-		print("Running simulation w/ input file: %s" % (sys.argv[1]))
+		print("Running simulation w/ input file: %s\n" % (sys.argv[1]))
 		sim(sys.argv[1])
 	else:
 		print("Error: no input file given")
